5dca69c03c6018eaf95a0159cafcdf03b0a646bc
Ruan Aragão <ruanaragao2@gmail.com>
2014-04-06 22:39:24 -0300

    update

--- pixeldraw.js
+++ pixeldraw.js
@@ -3,12 +3,21 @@ var drawb = document.getElementById('draw-box');
 var btnBlack = document.getElementById('btnBlack');
 var btnRed = document.getElementById('btnRed');
 var btnErase = document.getElementById('btnErase');
+var colorSelected = document.getElementById('color');
 
 //drawb.onclick = function(){
 //  alert(this.id);
 //};
 
-var colorPixel = 'Black';
+function setColor (attr) {
+  if (attr == 'erase') {
+    colorPixel = 'rgba(255,255,255,0)';
+  } 
+  if (attr == 'brush') {
+    colorPixel = colorSelected.value;
+  }
+}
+//var colorPixel = 'Black';
 
 function createGrid(value){
   var pixel = 24;
@@ -27,7 +36,7 @@ function createGrid(value){
 function paint(id, colorPixel){
   var element = document.getElementById(id);
   if(element.style.backgroundColor == colorPixel){
-    element.style.backgroundColor = 'rgba(255,155,155,1)';
+    element.style.backgroundColor = 'rgba(255,155,155,0)';
   } else {
     element.style.backgroundColor = colorPixel;
   }
